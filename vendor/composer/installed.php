99abebdb823bbe49be670f4f1486ea390b93ca0e
david s <ds@gmail.com>
2021-10-08 14:28:36 +0700

    contoh Library Sederhana Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'programmerzamannow/belajar-php-composer-hello',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programmerzamannow/belajar-php-composer-hello' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
